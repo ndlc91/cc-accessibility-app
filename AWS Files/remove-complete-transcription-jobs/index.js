b7c0be703ad20a6faf77eb0a43feb4e3bca6645b
Jack K <78181793+Modmaster2000@users.noreply.github.com>
2023-03-26 00:48:35 -0500

    Added remove complete transcription jobs from aws
    Added the remove complete transcription jobs lambda function from aws that removes completed transcription jobs from the aws transcribe to simplify viewing transcription jobs in progress/failed.

---
+++ AWS Files/remove-complete-transcription-jobs/index.js
@@ -0,0 +1,44 @@
+const aws = require('aws-sdk');
+
+const s3 = new aws.S3({ apiVersion: '2006-03-01' });
+
+const transcribeservice = new aws.TranscribeService();
+
+
+exports.handler = async (event, context) => {
+    //console.log('Received event:', JSON.stringify(event, null, 2));
+
+    // Get the object from the event and show its content type
+    const bucket = event.Records[0].s3.bucket.name;
+    const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+    const objParams = {
+        Bucket: bucket,
+        Key: key,
+    };
+    const link = 's3://' + bucket + '/' + key;
+    try {
+        const { ContentType } = await s3.getObject(objParams).promise();
+        console.log('CONTENT TYPE:', ContentType);
+        console.log('LINK:', link);
+    } catch (err) {
+        console.log(err);
+        const message = `Error getting object ${key} from bucket ${bucket}. Make sure they exist and your bucket is in the same region as this function.`;
+        console.log(message);
+        throw new Error(message);
+    }
+    
+    const idArray=key.split(".");
+    const jobName=idArray[0] + "." + idArray[1];
+    
+    const params = {
+        TranscriptionJobName: jobName,
+    };
+    try {
+        const {} = await transcribeservice.deleteTranscriptionJob(params).promise();
+    } catch (err) {
+        console.log(err);
+        const message = `Error with deleting transcription job for file ${key}.`;
+        console.log(message);
+        throw new Error(message);
+    }
+};

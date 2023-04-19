b02b783927ca98241d38ff73f3ce3ec56cf7513f
Modmaster2000 <Kempfinator@rocketmail.com>
2023-04-19 18:06:53 -0500

    Implemented dynamic transcript fetching
    App will now generate a transcript for a user input video, and the client will receive updates on the transcription status. Once the transcript is done, it will display it in the transcript div.

--- src/Components/TranscriptFetcher/TranscriptJobStatusFetcher.js
+++ src/Components/TranscriptFetcher/TranscriptJobStatusFetcher.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import TranscriptFetcher from "./TranscriptFetcher";
+
 // A function to get the status of a transcription job from aws
 const TranscriptJobStatusFetcher = ({ filename }) => {
 
@@ -8,29 +10,35 @@ const TranscriptJobStatusFetcher = ({ filename }) => {
     useEffect(() => {
         let interval = setInterval (() => {
             const API_ENDPOINT = `https://82odtjxlp5.execute-api.us-east-2.amazonaws.com/transcriptstatus?key=${filename}`;
-            //console.log(API_ENDPOINT);
+            
             axios.get(API_ENDPOINT)
                 .then((response) => {
                     setStatus(response.data)
-                    //console.log(response);
+                    
                 })
                 .catch((err) => {
                     console.log(err);
                 })
-                //console.log(status)
+                
         }, 15000); // runs the interval every 15000 milliseconds (15 seconds)
         return() => clearInterval(interval);
-    }, []);
+    }, [filename]);
     // returns the status after succesfully finding a job at the bottom of the transcript div
     if(status.statusCode === 200){
-        if(status.status === "FAILED"){
-           return <p>Status: {status.status}. {status.failureReason}</p>
+        if(status.status === "COMPLETED"){
+            return <TranscriptFetcher filename={filename}/> // COMPLETED
+        }
+        else if(status.status === "FAILED"){
+           return <p>Status: {status.status}. {status.failureReason}</p> // FAILED
         }
         else {
             return(
-                <p>Status: {status.status}</p>
+                <p>Status: {status.status}</p> // IN PROGRESS
             )
         } 
+    }
+    else {
+        return <p>Upload Video for Transcript to appear</p>
     }
 }
 
